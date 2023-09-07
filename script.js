ccae2fd98733ff339ea42004e3d470c892d47514
APWeiner <131618917+APWeiner@users.noreply.github.com>
2023-09-07 10:59:02 -0500

    Added drag and drop functionality in script.js

--- script.js
+++ script.js
@@ -14,6 +14,12 @@ function addTask(){
     else{
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
+        li.setAttribute("draggable", "true")
+
+        li.addEventListener("dragstart", dragStart);
+        li.addEventListener("dragover", dragOver);
+        li.addEventListener("drop", drop);
+
         listContainer.appendChild(li);
         let span = document.createElement("span");
         span.innerHTML = 'x';
@@ -34,10 +40,44 @@ listContainer.addEventListener("click", function(e){
     }
 }, false);
 
+var draggedItem = null;
+
+function dragStart(event) {
+  draggedItem = event.target;
+  event.dataTransfer.setData("text/plain", event.target.textContent);
+}
+
+function dragOver(event) {
+  event.preventDefault();
+}
+
+function drop(event) {
+  event.preventDefault();
+  if (draggedItem !== event.target) {
+    // Swap the positions of the dragged item and the drop target
+    var temp = event.target.innerHTML;
+    var tempClass = event.target.classList.contains("checked");
+
+    event.target.innerHTML = draggedItem.innerHTML;
+    event.target.classList.toggle("checked", draggedItem.classList.contains("checked"));
+
+    draggedItem.innerHTML = temp;
+    draggedItem.classList.toggle("checked", tempClass);
+    saveData()
+  }
+}
+
 function saveData(){
     localStorage.setItem("data", listContainer.innerHTML)
 }
 function showList(){
     listContainer.innerHTML = localStorage.getItem("data");
+    var liElements = listContainer.querySelectorAll("li")
+    liElements.forEach(function(li) {
+        li.setAttribute("draggable", "true");
+        li.addEventListener("dragstart", dragStart);
+        li.addEventListener("dragover", dragOver);
+        li.addEventListener("drop", drop);
+    });
 }
 showList();
